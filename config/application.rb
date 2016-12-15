74a7b77e6f16b0d2a5c4079c8acdd7256e2e715f
irony-irony <tomoya.isemura@gmail.com>
2016-12-15 20:40:29 +0900

    Edit application.rb
    Add: empty row

--- config/application.rb
+++ config/application.rb
@@ -22,3 +22,4 @@ config.generators do |g|
   g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
   g.fixture_replacement :factory_girl, dir: "spec/support/factories"
 end
+

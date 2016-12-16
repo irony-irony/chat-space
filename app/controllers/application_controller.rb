f3fa5cba5c6a6b7983627e79d2f6a9d84c4a35c5
irony-irony <tomoya.isemura@gmail.com>
2016-12-16 18:08:30 +0900

    Add root_path after sign_in and sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_parmitted_paramaters, if: :devise_controller?
 
   def configure_perimtted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
+
+
+  def after_sign_in_path_for(resources)#サインした後のリダイレクと先の指定
+    '/messages/'
+  end
+
+  def after_sign_out_path_for(resource) #サインアウトした後のリダイレクト先の指定
+    '/users/sign_in'
   end
 end

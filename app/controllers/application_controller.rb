6a586a43543619629c0bff3ffce91f521cfcc455
irony-irony <tomoya.isemura@gmail.com>
2016-12-16 17:39:36 +0900

    Add name column to user-table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_perimtted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 end

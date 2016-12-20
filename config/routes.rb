85b6d4d0a8e46e1c047636a6732440599351f5dc
irony-irony <tomoya.isemura@gmail.com>
2016-12-20 17:51:24 +0900

    Change route_pass, Flow (Log in --> group_index )

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-	resources :groups, only: [:new, :create, :edit, :update] do
-		resources :messages, only: [:index, :create, :show]
+  root 'groups#index'
+	resources :groups, only: [:index, :new, :create, :edit, :update] do
+		resources :messages, only: [:index, :create]
 	end
 end

29b8addefd299e1d0e2c6639d40cf3bfe0cffa3e
irony-irony <tomoya.isemura@gmail.com>
2016-12-20 17:23:34 +0900

    Nested message routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-resources :messages, only: [:index]
-resources :groups, only: [:new, :create, :edit, :update]
+	resources :groups, only: [:new, :create, :edit, :update] do
+		resources :messages, only: [:index, :create, :show]
+	end
 end

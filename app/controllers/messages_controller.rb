06cf45fb5f9d8a41c933b01ec864da965dce2a71
irony-irony <tomoya.isemura@gmail.com>
2016-12-21 20:46:26 +0900

    Edit message controller to show group name and member at the top

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
   def index
-
+    user = User.find(current_user.id)
+    @groups = user.groups
+    @group = Group.find(params[:group_id])
   end
 end
+

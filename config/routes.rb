bd933bc559a3ff23aa799cc67bbbce6f3cc24092
irony-irony <tomoya.isemura@gmail.com>
2016-12-22 18:05:35 +0900

    Simplify resources code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-	resources :groups, only: [:index, :new, :create, :edit, :update] do
-		resources :messages, only: [:index, :create]
+	resources :groups, only: %(index new create edit update) do
+		resources :messages, only: %i(index create)
 	end
 end

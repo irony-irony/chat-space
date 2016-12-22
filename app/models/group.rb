f63dcfbe9579b72893b9117e3ce9e729aa6b804b
irony-irony <tomoya.isemura@gmail.com>
2016-12-22 15:25:32 +0900

    Add validates for group model to restrict an empty group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
+  validates :group_name, presence: true
+  validates :user_ids, presence: true
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages

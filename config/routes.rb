008051625da7863ea83c89dcc3ccbd2550917a46
irony-irony <tomoya.isemura@gmail.com>
2016-12-22 19:29:49 +0900

    Modify mistype

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-	resources :groups, only: %(index new create edit update) do
+	resources :groups, only: %i(index new create edit update) do
 		resources :messages, only: %i(index create)
 	end
 end

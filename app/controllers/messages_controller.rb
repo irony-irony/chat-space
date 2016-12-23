2d3966dd8d8d59953cf97ebfa0f0ef97374490e9
irony-irony <tomoya.isemura@gmail.com>
2016-12-23 15:17:29 +0900

    Edit messages_controller to create messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,17 @@ class MessagesController < ApplicationController
     user = User.find(current_user.id)
     @groups = user.groups
     @group = Group.find(params[:group_id])
+    @message = Message.new
+  end
+
+  def create
+  	Message.create(message_params)
+    redirect_to group_messages_path(params[:group_id])
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
 

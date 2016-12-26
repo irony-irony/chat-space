ab820ca8cc9f6b3786bc3efdc532689cb42a4c20
irony-irony <tomoya.isemura@gmail.com>
2016-12-26 15:46:30 +0900

    Adjusted intend

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     it "is valid with a body. image, user_id, group_id, created_at and updated_at " do
-     message = build(:message)
+      message = build(:message)
       message.valid?
       expect(message).to be_valid
     end
 
     it "is valid without any image" do
-     message = build(:message, image: nil)
+      message = build(:message, image: nil)
       message.valid?
       expect(message).to be_valid
     end
 
     it "is invalid without any body" do
-     message = build(:message, body: nil)
+      message = build(:message, body: nil)
       message.valid?
       expect(message.errors[:body]).to include("本文が入力されていません")
     end
 
     it "is invalid without any user_id" do
-     message = build(:message, user_id: nil)
+      message = build(:message, user_id: nil)
       message.valid?
       expect(message.errors[:user_id]).to include("user_idがありません")
     end
 
     it "is invalid without any group_id" do
-     message = build(:message, group_id: nil)
+      message = build(:message, group_id: nil)
       message.valid?
       expect(message.errors[:group_id]).to include("group_idがありません")
     end

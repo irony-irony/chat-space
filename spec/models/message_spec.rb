efc7a648fde907b3be56a8b9296bfdd2df4a6624
irony-irony <tomoya.isemura@gmail.com>
2016-12-26 15:29:05 +0900

    Add test for validation body, use_id and group_id nil

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -13,5 +13,23 @@ describe Message do
       message.valid?
       expect(message).to be_valid
     end
+
+    it "is invalid without any body" do
+     message = build(:message, body: nil)
+      message.valid?
+      expect(message.errors[:body]).to include("本文が入力されていません")
+    end
+
+    it "is invalid without any user_id" do
+     message = build(:message, user_id: nil)
+      message.valid?
+      expect(message.errors[:user_id]).to include("user_idがありません")
+    end
+
+    it "is invalid without any group_id" do
+     message = build(:message, group_id: nil)
+      message.valid?
+      expect(message.errors[:group_id]).to include("group_idがありません")
+    end
   end
 end

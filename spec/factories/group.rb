eb6c8a07a0ef81ef721b70472917dc913c1e7229
irony-irony <tomoya.isemura@gmail.com>
2017-01-05 12:31:20 +0900

    Edit factory group

---
+++ spec/factories/group.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :group do
+    group_name            "グループ1"
+    created_at            "2016-12-24 11:11:11"
+    updated_at            "2016-12-24 11:11:11"
+  end
+end

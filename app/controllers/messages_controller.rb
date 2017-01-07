fd645c946b1382b9d0d845446e21d76f6fc6fe47
irony-irony <tomoya.isemura@gmail.com>
2017-01-07 15:48:43 +0900

    Edit messages_controller for asynchronously sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,17 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.create(message_params)
-    redirect_to group_messages_path(params[:group_id])
+    @message = Message.new (message_params)
     if @message.save
-      flash[:notice] = "メッセージが送信されました"
+      respond_to do |format|
+        format.html { redirect_to group_messages_path, notice: "メッセージが送信されました" }
+        format.json { render json: @message}
+      end
     else
-      flash[:alert] = "空欄のままでは送信できません"
+      respond_to do |format|
+        format.html { redirect_to group_messages_path, alert: "メッセージが送信できませんでした" }
+        format.json { render json: { alert: 'error'} }
+      end
     end
   end
 

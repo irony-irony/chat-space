b545fd3b24225463176b3123c10df1e513f2dce2
irony-irony <tomoya.isemura@gmail.com>
2017-01-09 19:38:04 +0900

    Edit for apply time-formats

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,11 @@ class MessagesController < ApplicationController
     @message = Message.new (message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_messages_path, notice: "メッセージが送信されました" }
+        format.html { redirect_to group_messages_path }
         format.json { render json: {
             name: current_user.name,
-            time: @message.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-            body: @message.body
+            time: @message.created_at.to_formatted_s(:datetime),
+            body: @message.body,
           }
         }
       end

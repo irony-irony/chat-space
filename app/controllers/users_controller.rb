4babc0e410fa3638d57df254064e31898b631384
irony-irony <tomoya.isemura@gmail.com>
2017-01-11 20:37:38 +0900

    Edit users_controller, add search function, only json applied

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  def edit
-
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.json {render json: @users}
+    end
   end
 end

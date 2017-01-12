2913a18145683e77c00448a62985f791d5cc89b5
irony-irony <tomoya.isemura@gmail.com>
2017-01-12 15:21:49 +0900

    Edit routes, delete only: search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: :search do
+  resources :users do
     collection do
       get 'search'
     end

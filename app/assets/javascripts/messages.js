89acd4422f95b0e9e8de7defb0622bc2c5c82ff6
irony-irony <tomoya.isemura@gmail.com>
2017-01-18 17:55:20 +0900

    Add auto refresh function

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,11 +1,16 @@
 $(function(){
+  $.ajaxSetup({
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+    }
+  });
+
   function buildHTML(data) {
     if (data.image) {
-      var insertImage = '<br><img src="' + data.image + '">';
+      var insertImage = '<br><img src="' + data.image.url + '">';
       } else {
       var insertImage = '';
       }
-    }
     var html =  '<li class = "chat-message">'+
                 '<div class = "chat-message__header clearfix">'+
                 '<p class = "chat-message__name">'+ data.name +
@@ -14,7 +19,8 @@ $(function(){
                 '<p class = "chat-message__body">'+ data.body + insertImage +
                 '</p></li>';
     return html;
-  }
+  };
+
 
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
@@ -23,7 +29,7 @@ $(function(){
     var fd = new FormData($(this)[0]);
     $.ajax({
       type: 'POST',
-      url: window.location.href + '.json',
+      url: window.location.href,
       data: fd,
       processData: false,
       contentType: false,
@@ -38,4 +44,32 @@ $(function(){
       alert('error');
     });
   });
+
+   // URLがgroups/num/messagesのページに於いて自動更新
+  var searchUrl = document.location.href.match(/\/groups\/\d+\/messages/);
+  if (searchUrl) {
+    setInterval(function(){
+      $.ajax({
+        url: window.location.href + '.json',
+        method: 'GET',
+        dataType: 'json'
+      })
+      .done(function(data){
+        console.log(data);
+        var dbCount = data.db.length;
+        var screenCount = $('li.chat-message').length;
+        if (screenCount != dbCount) {
+          $('li.chat-message').remove();
+          for (var i = 0; i < dbCount; i++) {
+            var html = buildHTML(data.db[i]);
+            $('ul.chat-messages').append(html);
+            $('.chat-group__link').find('p.chat-group__message').text(data.db[i].body);
+          }
+        }
+      })
+      .fail(function(){
+        alert('error');
+      });
+    }, 10 * 1000);
+  }
 });
